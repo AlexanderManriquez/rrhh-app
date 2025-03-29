df283523652f79f24a547b546faf825d175b0558
Alexander Manriquez <Alexander.manriquez.s@gmail.com>
2025-03-29 20:01:37 -0300

    Add POST endpoint to EmpleadoControlador for adding new employees

--- rrhh-java-spring/src/main/java/am/rrhh/controlador/EmpleadoControlador.java
+++ rrhh-java-spring/src/main/java/am/rrhh/controlador/EmpleadoControlador.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +32,9 @@ public class EmpleadoControlador {
         return empleados;
     }
 
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
+        logger.info("Empleado a agregar: " + empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
 }
