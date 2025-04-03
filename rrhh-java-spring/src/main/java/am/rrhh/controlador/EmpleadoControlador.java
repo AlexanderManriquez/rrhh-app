f03bd0af83f8d5455a8e711fd4c8abb65f9bcb91
Alexander Manriquez <Alexander.manriquez.s@gmail.com>
2025-04-03 19:19:15 -0300

    Add endpoint to retrieve employee by ID and create custom exception for not found resources

--- rrhh-java-spring/src/main/java/am/rrhh/controlador/EmpleadoControlador.java
+++ rrhh-java-spring/src/main/java/am/rrhh/controlador/EmpleadoControlador.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import am.rrhh.excepcion.RecursoNoEncontradoExcepcion;
 import am.rrhh.modelo.Empleado;
 import am.rrhh.servicio.IEmpleadoServicio;
 
@@ -36,5 +39,13 @@ public class EmpleadoControlador {
     public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if(empleado == null)
+            throw new RecursoNoEncontradoExcepcion("Empleado no encontrado con id: " + id);
+        return ResponseEntity.ok(empleado);
     }
 }
